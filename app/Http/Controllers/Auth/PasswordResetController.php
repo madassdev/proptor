7be92c89a67c783ea24d44614d9a08dbcf952966
lordfaves <favescsskr@gmail.com>
2020-09-25 12:19:47 +0100

    OAUTH KEYS

--- app/Http/Controllers/Auth/PasswordResetController.php
+++ app/Http/Controllers/Auth/PasswordResetController.php
@@ -61,7 +61,7 @@ class PasswordResetController extends Controller
         
         $generated_password = strtolower(Str::random(8));
         $user->password = bcrypt($generated_password);
-        $token =  $user->createToken('ngcart-token')->accessToken;
+        $token =  $user->createToken('proptor-token')->accessToken;
         $user->save();
         
         // Delete the reset token
@@ -77,70 +77,3 @@ class PasswordResetController extends Controller
 
 }
 
-class PasswoardResetController extends Controller
-{
-    public function find($token)
-    {
-        $passwordReset = PasswordReset::where('token', $token)
-            ->first();
-        if (!$passwordReset)
-            return response()->json([
-                'success'=>false,
-                'data'=>['message' => 'This password reset token is invalid.']
-            ], 404);
-        if (Carbon::parse($passwordReset->updated_at)->addMinutes(720)->isPast()) {
-            $passwordReset->delete();
-            return response()->json([
-                'success'=>false,
-                'data'=>['message' => 'This password reset token is invalid.']
-            ], 404);
-        }
-        return response()->json($passwordReset);
-    }
-     
-    public function reset(Request $request)
-    {
-        $validator = Validator::make($request->all(), [ 
-            'email' => 'required|string|email',
-            'token' => 'required|string'
-        ]);
-
-        if ($validator->fails()) { 
-            
-            return response()->json(['success'=>false, 'data'=>$validator->errors()], 406);
-        }
-
-        
-        $passwordReset = PasswordReset::where([
-            ['token', $request->token],
-            ['email', $request->email]
-        ])->first();
-
-        if (!$passwordReset)
-            return response()->json([
-                'success' => false,
-                'data' => ['message'=>'The password reset token is invalid.']
-            ], 403);
-
-        $user = User::where('email', $passwordReset->email)->first();
-        if (!$user)
-            return response()->json([
-                'success' => false,
-                'data' => ['message'=>'We cannot find a user with that e-mail address.']
-            ], 404);
-
-        $generated_password = strtolower(Str::random(8));
-        $user->password = bcrypt($generated_password);
-        $token =  $user->createToken('ngcart-token')->accessToken;
-        $user->save();
-        $passwordReset->delete();
-        $user->notify(new PasswordResetSuccess($generated_password));
-        return response()->json([
-                                'success'=>true,
-                                'data'=>[
-                                    'user'=>$user,
-                                    'token'=>$token,
-                                    ]
-                            ], 200);
-    }
-}
