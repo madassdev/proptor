1946639e33af4c01d676493f16109643e1e5df01
lordfaves <favescsskr@gmail.com>
2020-10-03 17:35:14 +0100

    Commit admin application to Iteam

--- database/migrations/prop/2020_09_18_102457_create_payments_table.php
+++ database/migrations/prop/2020_09_18_102457_create_payments_table.php
@@ -22,6 +22,7 @@ class CreatePaymentsTable extends Migration
             $table->string('reference');
             $table->string('status')->default('pending');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
